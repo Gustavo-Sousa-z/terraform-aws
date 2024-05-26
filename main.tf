c7354e2710b5dabe6753d14d33dbc0e602634c48
Gustavo Sousa <gcrsousa@hotmail.com>
2024-05-26 20:40:12 -0300

    Provider/backen configurado && criação da VPC

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.51.1"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform-labs-state"
+    key    = "prd/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  profile = "terraform"
+}
---
+++ vpc.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc" "primeira_VPC" {
+  cidr_block         = "10.1.0.0/16"
+  enable_dns_support = false
+
+  tags = {
+    Name       = "Primeira_VPC"
+    "Criado"   = "Pelo terraform"
+    "State S3" = "Alterado"
+  }
+}
