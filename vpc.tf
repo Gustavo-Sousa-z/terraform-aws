1808e123e6a02207a3eb11c1fb97dc05a0de0941
Gustavo Sousa <gcrsousa@hotmail.com>
2024-05-28 22:01:25 -0300

    Configurado variaveis para cidr da VPC, uso de data para AZ e configurado locals para tags. E configurado 5 subnets para criacao

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,65 @@
-resource "aws_vpc" "primeira_VPC" {
-  cidr_block         = "10.0.0.0/16"
-  enable_dns_support = false
+resource "aws_vpc" "eks_vpc" {
+  cidr_block           = var.cidr_block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
-    Name       = "Primeira_VPC"
-    "Criado"   = "Pelo terraform"
-    "State S3" = "Alterado"
+    Name = "eks_vpc"
   }
+}
+#Subnets Publicas AZ-A e AZ-B
+resource "aws_subnet" "eks_subnet_public_1a" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
+  availability_zone       = "${data.aws_region.current.name}a" #Função para calcular o cidr_block da subnet a partir da VPC
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "public-subnet-eks-1a"
+    }
+  )
+}
+
+resource "aws_subnet" "eks_subnet_public_1b" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
+  availability_zone       = "${data.aws_region.current.name}b" #Função para calcular o cidr_block da subnet a partir da VPC
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "public-subnet-eks-1b"
+    }
+  )
+}
+
+#Subnets Privadas AZ-A e AZ-B
+resource "aws_subnet" "eks_subnet_private_1a" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 3) #Função para calcular o cidr_block da subnet a partir da VPC
+  availability_zone = "${data.aws_region.current.name}a"
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "private-subnet-eks-1a"
+    }
+  )
+}
+
+resource "aws_subnet" "eks_subnet_private_1b" {
+  vpc_id            = aws_vpc.eks_vpc.id
+  cidr_block        = cidrsubnet(var.cidr_block, 8, 4)
+  availability_zone = "${data.aws_region.current.name}b" #Função para calcular o cidr_block da subnet a partir da VPC
+
+  #A função merge esta contatenando todas as tags criadas no locals com minhas tags próprias
+  tags = merge(
+    local.tags,
+    {
+      Name = "private-subnet-eks-1b"
+    }
+  )
 }
